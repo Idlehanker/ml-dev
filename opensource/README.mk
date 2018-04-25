27965c66cd903c61f04daa3cf48258a054c89fb1
huzj <huzj2@cmschina.com.cn>
2018-04-25 08:36:26 +0800

    up

--- opensource/README.mk
+++ opensource/README.mk
@@ -1,3 +1,3 @@
 https://github.com/deeplearning4j/deeplearning4j.git
-https://github.com/deeplearning4j/deeplearning4j.git
 https://github.com/aymericdamien/TensorFlow-Examples.git
+https://github.com/tensorflow/tensorflow.git
